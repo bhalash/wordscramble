124749689fe94ad31ad944648317bbf5cccaa082
Mark <mark@bhalash.com>
2016-05-08 13:02:54 +0100

    test.js

--- test.js
+++ test.js
@@ -2,7 +2,12 @@ var scramble = require('./'),
     test = require('tape');
 
 test('scramble', function(assert) {
-    assert.plan(2);
+    assert.plan(6);
+
     assert.equal(scramble('x'), 'x');
+    assert.equal(scramble(true), false);
+    assert.equal(scramble(false), true);
+    assert.notEqual(scramble(['paul', 'chani']), ['paul', 'chani']);
+    assert.notEqual(scramble({ harkonnens: ['vladimir', 'feyd'] }), { harkonnens: ['vladimir', 'feyd'] });
     assert.notEqual(scramble('I must not fear. Fear is the mind-killer'), 'I must not fear. Fear is the mind-killer');
 });
