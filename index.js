66fd6159d02749c7f55fe3f99cfd1f72fe4450ba
Mark <mark@bhalash.com>
2016-05-08 12:09:08 +0100

    Expanded scope of scramble.js to most common JavaScript values.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-module.exports = require('./scramble');
+'use strict';
+
+/**
+ * Return a string scrambled randomly.
+ *
+ * @param {String} string
+ * @returns {String} letters
+ */
+
+module.exports = function(string) {
+    if (typeof string !== 'string') {
+        throw new Error('Passed value must be a string!');
+    }
+
+    var letters = string.split(''),
+        index = letters.length,
+        temp = '',
+        random = 0;
+
+    while (--index > 0) {
+        random = Math.floor(Math.random() * index);
+
+        temp = letters[index];
+        letters[index] = letters[random];
+        letters[random] = temp;
+    }
+
+    return letters.join('');
+}
--- scramble.js
+++ scramble.js
@@ -1,29 +1,86 @@
 'use strict';
+var scramble = {};
 
 /**
- * Return a string scrambled randomly.
+ * Generically scramble a JavaScript object.
+ * Float and integer value are treated as strings.
  *
- * @param {String} string
- * @returns {String} letters
+ * @param {object} victim - A pristine, innocent JavaScript value.
+ * @returns {object} victim - A molested, scrambled JavaScript value.
  */
 
-module.exports = function(string) {
-    if (typeof string !== 'string') {
-        throw new Error('Passed value must be a string!');
+scramble.shuffle = function(victim) {
+    if (Array.isArray(victim)) {
+        victim = scramble.array.call(null, victim.map(scramble.shuffle));
+    } else if (typeof(victim) === 'object') {
+        victim = scramble.object(victim);
+    } else if (typeof(victim) === 'boolean') {
+        victim = scramble.bool(victim);
+    } else {
+        victim = scramble.string(victim);
     }
 
-    var letters = string.split(''),
-        index = letters.length,
-        temp = '',
-        random = 0;
+    return victim;
+}
+
+/**
+ * Scramble the order of elements in an array.
+ *
+ * @param {array} victim
+ * @returns {array} victim
+ */
+
+scramble.array = function(victim) {
+    var index = victim.length,
+        random = 0,
+        temp = '';
 
     while (--index > 0) {
         random = Math.floor(Math.random() * index);
 
-        temp = letters[index];
-        letters[index] = letters[random];
-        letters[random] = temp;
+        temp = victim[index];
+        victim[index] = victim[random];
+        victim[random] = temp;
     }
 
-    return letters.join('');
+    return victim;
 }
+
+/**
+ * Scramble a string/number.
+ *
+ * @param {string | number} victim
+ * @returns {string | number} victim
+ */
+
+scramble.string = function(victim) {
+    return scramble.array.call(null, String(victim).split('')).join('');
+}
+
+/**
+ * Scramble a boolean.
+ *
+ * @param {bool} victim
+ * @returns {bool} victim
+ */
+
+scramble.bool = function(victim) {
+    return !victim;
+}
+
+/**
+ * Scramble an object.
+ *
+ * @param {object} victim
+ * @returns {object} victim
+ */
+
+scramble.object = function(victim) {
+    Object.keys(victim).forEach(key => {
+        victim[key] = scramble.shuffle(victim[key]);
+    });
+
+    return victim;
+}
+
+module.exports = scramble.shuffle;
