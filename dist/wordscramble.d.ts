fb3a44a812af10718067e5ca290de399d277aff0
Mark <mark@bhalash.com>
2016-08-21 15:18:10 +0100

    Removed TypeScript definition files.

--- dist/wordscramble.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Wordscramble {
-    shuffle(victim: any): any;
-    array(victim: any[]): any[];
-    boolean(victim: boolean): boolean;
-    number(victim: number): number;
-    object(victim: any): any;
-    string(victim: string): string;
-}
