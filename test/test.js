e092ad70bf317ffff691cafbb7f410156a3ac53d
Mark <mark@bhalash.com>
2016-08-28 19:35:27 +0100

    Updated test suite.

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
-var scramble = new (require('./')).Wordscramble();
+var ws = require('../dist').ws;
     test = require('tape');
 
-test('scramble', function(assert) {
+test('Wordscramble', function(assert) {
     assert.plan(6);
 
-    assert.equal(scramble('x'), 'x');
-    assert.equal(scramble(true), false);
-    assert.equal(scramble(false), true);
-    assert.notEqual(scramble(['paul', 'chani']), ['paul', 'chani']);
-    assert.notEqual(scramble({ harkonnens: ['vladimir', 'feyd'] }), { harkonnens: ['vladimir', 'feyd'] });
-    assert.notEqual(scramble('I must not fear. Fear is the mind-killer'), 'I must not fear. Fear is the mind-killer');
+    assert.equal(ws.scramble('x'), 'x');
+    assert.equal(ws.scramble(true), false);
+    assert.equal(ws.scramble(false), true);
+    assert.notEqual(ws.scramble(['paul', 'chani']), ['paul', 'chani']);
+    assert.notEqual(ws.scramble({ harkonnens: ['vladimir', 'feyd'] }), { harkonnens: ['vladimir', 'feyd'] });
+    assert.notEqual(ws.scramble('I must not fear. Fear is the mind-killer'), 'I must not fear. Fear is the mind-killer');
 });
